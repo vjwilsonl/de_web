3de142d5936a618ac664e97e99ef4efb1561ec4f
vjwilsonl <vjwilsonl@gmail.com>
2018-07-25 16:21:03 +0800

    Home page update

--- index.php
+++ index.php
@@ -32,7 +32,10 @@ get_header();
 					<!-- News Section -->
 					<?php get_template_part( 'template-parts/home-news' ); ?>
 					<!-- End of News Section -->
+					<!-- Social media -->
+
 				</div>
+				<?php get_template_part( 'template-parts/home-social' ); ?>
 			</div>
 		</div>
 		<!-- End Of Features News Social -->
---
+++ template-parts/home-social.php
@@ -0,0 +1,15 @@
+<!-- Social media feed -->
+<aside class="col-sm-12 col-lg-4">
+	<div class="row">
+		<div class="col-12 section-title">
+			<h1>Social</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12 mt-4">
+			<div class="twitter-wrapper">
+				<a class="twitter-timeline" data-height="700" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</div>
+		</div>
+	</div>
+</aside>
