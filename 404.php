2e5b98ba27a7083427af261c008a8e4978f424b4
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2018-08-07 18:25:12 +0800

    Changed Template for 404 page

--- 404.php
+++ 404.php
@@ -10,51 +10,22 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<?php get_header(); ?>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'difference-engine' ); ?></h1>
-				</header><!-- .page-header -->
+    <main>
+        <div class="container">
+            <section class="section-blogsingle">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="text-center">OOPS! THAT PAGE CAN’T BE FOUND.</h1>
+                        <p class="text-center">It looks like nothing was found at this location.</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'difference-engine' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'difference-engine' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$difference_engine_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'difference-engine' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$difference_engine_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<?php get_footer(); ?>
 
 <?php
 get_footer();
