d5f74cfa4e3afb1314ceee02c75009810394973a
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2018-08-13 15:21:48 +0800

    Changed News Archive Layout

--- archive-news.php
+++ archive-news.php
@@ -9,52 +9,51 @@
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card-columns">
+                <div class="row blog-block mt-4">
+
+                    <?php
+                        $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+
+                        $args = array(
+                            'post_type'         => 'news',
+                            'posts_per_page'    => 6,
+                            'order'             => 'DESC',
+                            'orderby'           => 'ID',
+                            'paged'             => $paged,
+                            'page'              => $paged
+                        );
+
+                        $news = new WP_Query( $args );
+                        if ( $news->have_posts() ) :
+                            while ( $news->have_posts() ) : $news->the_post();
+
+                                $trimmed_content = wp_filter_nohtml_kses( wp_trim_words( get_the_content(), 25, "" ) );
+                                $publish_date = get_the_date( 'F j, Y');
+                                ?>
+
+                                <div class="col-12 col-md-6 col-lg-4">
+                                    <div class="card">
+                                        <img class="card-img-top" src="<?= get_the_post_thumbnail_url() ?>" alt="<?= the_title(); ?>">
+                                        <a class="blog-caption hvr-underline-from-left" href="<?= get_post_permalink() ?>">
+                                            <div class="card-body p-1">
+                                                <p class="card-text"><?= $publish_date ?></p>
+                                                <h3 class="card-title"><?= the_title(); ?></h3>
+                                                <p class="card-text"><?= $trimmed_content; ?> (...)</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
 
                             <?php
-                                $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+                            endwhile;
+                        else: ?>
 
-                                $args = array(
-                                    'post_type'         => 'news',
-                                    'posts_per_page'    => 6,
-                                    'order'             => 'DESC',
-                                    'orderby'           => 'ID',
-                                    'paged'             => $paged,
-                                    'page'              => $paged
-                                );
+                            <p>There are no news published yet. Come back later.</p>
 
-                                $news = new WP_Query( $args );
-                                if ( $news->have_posts() ) :
-                                    while ( $news->have_posts() ) : $news->the_post();
+                        <?php endif;
 
-                                        $trimmed_content = wp_filter_nohtml_kses( wp_trim_words( get_the_content(), 25, "" ) );
-                                        $publish_date = get_the_date( 'F j, Y');
-                                        ?>
+                    ?>
 
-                                        <div class="card">
-                                            <img class="card-img-top" src="<?= get_the_post_thumbnail_url() ?>" alt="<?= the_title(); ?>">
-                                            <a class="blog-caption hvr-underline-from-left" href="<?= get_post_permalink() ?>">
-                                                <div class="card-body p-1">
-                                                    <p class="card-text"><?= $publish_date ?></p>
-                                                    <h5 class="card-title"><?= the_title(); ?></h5>
-                                                    <p class="card-text"><?= $trimmed_content; ?> (...)</p>
-                                                </div>
-                                            </a>
-                                        </div>
-
-                                    <?php
-                                    endwhile;
-                                else: ?>
-
-                                    <p>There are no news published yet. Come back later.</p>
-
-                                <?php endif;
-
-                            ?>
-                        </div>
-                    </div>
                 </div>
 
                 <!-- Page Navigation -->
