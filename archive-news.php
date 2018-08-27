a528aa8953c0083eb7a031ba9aa797a85b0b57eb
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2018-08-27 16:18:08 +0800

    Added Tags on News Archive Page

--- archive-news.php
+++ archive-news.php
@@ -14,8 +14,14 @@
                     <?php
                         $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
 
+                        $tag_name = '';
+                        if(isset( $_GET['tag'])):
+                            $tag_name = $_GET['tag'];
+                        endif;
+
                         $args = array(
                             'post_type'         => 'news',
+                            'tag'               => $tag_name,
                             'posts_per_page'    => 6,
                             'order'             => 'DESC',
                             'orderby'           => 'ID',
@@ -29,18 +35,36 @@
 
                                 $trimmed_content = wp_filter_nohtml_kses( wp_trim_words( get_the_content(), 25, "" ) );
                                 $publish_date = get_the_date( 'F j, Y');
+                                $post_tags = get_the_tags();
                                 ?>
 
                                 <div class="col-12 col-md-6 col-lg-4">
                                     <div class="card">
                                         <img class="card-img-top" src="<?= get_the_post_thumbnail_url() ?>" alt="<?= the_title(); ?>">
-                                        <a class="blog-caption hvr-underline-from-left" href="<?= get_post_permalink() ?>">
+                                        <div class="blog-caption">
                                             <div class="card-body p-1">
                                                 <p class="card-text"><?= $publish_date ?></p>
-                                                <h3 class="card-title"><?= the_title(); ?></h3>
+                                                <h3 class="card-title"><a class="hvr-highlight hvr-highlight-thick" href="<?= get_post_permalink() ?>"><?= the_title(); ?></a></h3>
                                                 <p class="card-text"><?= $trimmed_content; ?> (...)</p>
                                             </div>
-                                        </a>
+
+                                            <div class="card-tags mt-3">
+                                                <?php
+                                                    if ( $post_tags ):
+                                                        echo "Tags: ";
+                                                        $last_tag = end(array_keys($post_tags));
+                                                        foreach( $post_tags as $key => $value ):
+                                                            if($key == $last_tag):
+                                                                echo "<a href='" . get_post_type_archive_link( 'news' ) ?>?tag=<?= $value->slug . "'>" . $value->name . "</a>";
+                                                            else:
+                                                                echo "<a href='" . get_post_type_archive_link( 'news' ) ?>?tag=<?= $value->slug . "'>" . $value->name . "</a>" . ", ";
+                                                            endif;
+                                                        endforeach;
+                                                    endif;
+                                                ?>
+                                            </div>
+                                        </div>
+
                                     </div>
                                 </div>
 
