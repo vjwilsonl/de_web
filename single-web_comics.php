326fa00b7c2f03691a1243ce8cd633dcbdedfc1c
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2019-04-16 18:01:13 +0800

    Working On Single Webcomic

--- single-web_comics.php
+++ single-web_comics.php
@@ -1,33 +1,33 @@
 <?php
-/**
- * The template for displaying web comics single page
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package Difference_Engine
- */
-while ( have_posts() ) :
-	the_post();
-	$creator_arr = get_field('creator');
-	$chapter_arr = get_field('chapter');
-	if (!empty($chapter_arr)) {
-		$chapter_arr_rev = array_reverse($chapter_arr);
+    /**
+     * The template for displaying web comics single page
+     *
+     * @link    https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+     *
+     * @package Difference_Engine
+     */
 
-	}
+    while( have_posts() ) :
+        the_post();
+        $creator_arr = get_field( 'creator' );
+        $chapter_arr = get_field( 'chapter' );
+        if ( ! empty( $chapter_arr ) ) {
+            $chapter_arr_rev = array_reverse( $chapter_arr );
+        }
 
-	if (isset($_GET['chapter'])) {
-		$chapter = strip_tags( trim( $_GET['chapter']));
-		$valid_chapter = (count($chapter_arr) >= $chapter && !( $chapter <= 0)) ? true : false ;
-	}
-?>
-			<?php if ($valid_chapter) : ?> <!-- show reader -->
-				<?php include( locate_template( 'template-parts/webcomic/chapter.php', false, false )); ?>
-				<?php get_footer(); ?>
-			<?php else: ?> <!-- show web comic info -->
-				<?php get_header(); ?>
-				<?php include( locate_template( 'template-parts/webcomic/info.php', false, false )); ?>
-				<?php get_footer(); ?>
-			<?php endif; ?>
-<?php
-endwhile; // End of the loop.
+        if ( isset( $_GET['chapter'] ) ) {
+            $chapter       = strip_tags( trim( $_GET['chapter'] ) );
+            $valid_chapter = ( count( $chapter_arr ) >= $chapter && ! ( $chapter <= 0 ) ) ? TRUE : FALSE;
+        }
+        ?>
+        <?php if ( $valid_chapter ) : ?> <!-- show reader -->
+        <?php include( locate_template( 'template-parts/webcomic/chapter.php', FALSE, FALSE ) ); ?>
+        <?php get_footer(); ?>
+    <?php else: ?> <!-- show web comic info -->
+        <?php get_header(); ?>
+        <?php include( locate_template( 'template-parts/webcomic/info.php', FALSE, FALSE ) ); ?>
+        <?php get_footer(); ?>
+    <?php endif; ?>
+    <?php
+    endwhile; // End of the loop.
 
--- template-parts/webcomic/info.php
+++ template-parts/webcomic/info.php
@@ -1,98 +1,133 @@
 <!-- display details and description of web comics -->
 <div id="primary" class="content-area">
-	<main id="main" class="site-main">
-		<main>
-			<div class="container-fluid no-gutters px-0">
-				<div class="row no-gutters">
-					<div class="wc-banner">
-						<img class="w-100" src="<?= (!empty(get_field('banner_image'))) ? get_field('banner_image')['url'] : '' ?>">
-					</div>
-				</div>
-			</div>
-			<div class="container">
-				<section class="section-wc-single">
-					<div class="row">
-						<div class="wc-banner">
-							<img src="">
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-12 col-lg-8">
-							<div class="row mb-2">
-								<div class="col-12 section-title">
-									<h1><?= the_title(); ?></h1>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-12">
-									<div class="wc-content-wrapper">
-										<?= the_content(); ?>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-12">
-									<div class="wc-share">
-										<h6>Share:</h6>
-										<ul class="list-inline">
-											<li class="list-inline-item"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" title="Share on Facebook."><i class="fab fa-facebook-f circle-icon"></i></a></li>
-											<li class="list-inline-item"><a href="https://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" target="_blank" title="Tweet this!"><i class="fab fa-twitter circle-icon"></i></a></li>
-											<li class="list-inline-item"><a class="copy-link" data-clipboard-text="<?php the_permalink(); ?>" tabindex="0" data-trigger="click" data-toggle="tooltip" title="Copy Link"><i class="fas fa-link circle-icon"></i></a></li>
-											<li class="list-inline-item"><a href="mailto:?subject=Look at this website&body=Hi, I found this website and thought you might like it <?php the_title(); ?> - <?php the_permalink(); ?>" target=""><i class="fas fa-envelope circle-icon"></i></a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
+    <main id="main" class="site-main">
+        <main>
+            <div class="container">
+                <div class="row no-gutters">
+                    <div class="wc-banner">
+                        <img class="w-100" alt="<?= the_title(); ?>" src="<?= ( ! empty( get_field( 'banner_image' ) ) ) ? get_field( 'banner_image' )['url'] : '' ?>">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <section class="section-webcomic-single">
+                    <div class="row">
+                        <div class="col-12 col-lg-9">
+                            <div class="row mb-2">
+                                <div class="col-12 section-title">
+                                    <h1><?= the_title(); ?></h1>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="wc-content-wrapper">
+                                        <?= the_content(); ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="share-buttons">
+                                        <ul class="list-inline">
+                                            <li class="list-inline-item" style="width: 100px;"><p>Share this comic!</p></li>
+                                            <li class="list-inline-item">
+                                                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" title="Share on Facebook.">
+                                                    <span class="fa-stack">
+                                                        <i class="fal fa-circle fa-stack-2x"></i>
+                                                        <i class="fab fa-facebook-f fa-stack-1x"></i>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="https://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" target="_blank" title="Tweet this!">
+                                                    <span class="fa-stack">
+                                                        <i class="fal fa-circle fa-stack-2x"></i>
+                                                        <i class="fab fa-twitter fa-stack-1x"></i>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a class="copy-link" data-clipboard-text="<?php the_permalink(); ?>" tabindex="0" data-trigger="click" data-toggle="tooltip" title="Copy Link">
+                                                    <span class="fa-stack">
+                                                        <i class="fal fa-circle fa-stack-2x"></i>
+                                                        <i class="fas fa-link fa-stack-1x"></i>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="mailto:?subject=Look at this website&body=Hi, I found this website and thought you might like it <?php the_title(); ?> - <?php the_permalink(); ?>" target="">
+                                                    <span class="fa-stack">
+                                                        <i class="fal fa-circle fa-stack-2x"></i>
+                                                        <i class="fas fa-envelope fa-stack-1x"></i>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
 
-							<!-- Chapter links -->
-							<?php if ( is_array($chapter_arr_rev) && count($chapter_arr_rev)): ?>
-								<div id="chapters" class="row">
-									<div class="col-12">
-										<div class="wc-chapter-wrapper mt-4">
-											<?php foreach ($chapter_arr_rev as $key => $value): ?>
-												<div class="row no-gutters mb-4">
-													<div class="col-sm-12 col-lg-4">
-														<img class="w-100" src="<?= ($value['chapter_image']['url']) ? $value['chapter_image']['url'] : '' ; ?>">
-													</div>
-													<a class="col-sm-12 col-lg-8 chapter-link chapter-link--primary chapter-link-animated" href="?chapter=<?= count($chapter_arr) - $key; ?>">
-														<h6><?= $value['chapter_title']; ?> <?= (!$key) ? '(Last Chapter)' : '' ; ?></h6><!-- <span class="up">UP</span> -->
-													</a>
-												</div>
-											<?php endforeach; ?>
-										</div>
-									</div>
-								</div>
-							<?php endif; ?>
-						</div>
+                            <!-- Chapter links -->
+                            <?php if ( is_array( $chapter_arr_rev ) && count( $chapter_arr_rev ) ): ?>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="webcomic-chapters-wrapper">
+                                            <?php foreach ( $chapter_arr_rev as $key => $value ): ?>
+                                                <div class="row no-gutters webcomic-chapter">
+                                                    <div class="col-sm-12 col-lg-5 chapter-cover">
+                                                        <img alt="<?= $value['chapter_title']; ?>" class="chapter-image"  src="<?= ( $value['chapter_image']['url'] ) ? $value['chapter_image']['url'] : ''; ?>">
+                                                    </div>
+                                                    <div class="col-sm-12 col-lg-7 chapter-body">
+                                                        <div class="row no-gutters">
+                                                            <div class="col-8 webcomic-title" >
+                                                                <p class="chapter-no"><?= $value['chapter_no']; ?> <?= ( ! $key ) ? '(Last Chapter)' : ''; ?></p><!-- <span class="up">UP</span> -->
+                                                                <h6 class="chapter-title"><?= $value['chapter_title']; ?></h6>
+                                                            </div>
+                                                            <div class="col-4">
+                                                                <p class="text-right align-middle chapter-read">
+                                                                    <a href="?chapter=<?= count( $chapter_arr ) - $key; ?>">
+                                                                        READ &nbsp; <i class="fas fa-arrow-right"></i>
+                                                                    </a>
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            <?php endforeach; ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
+                        </div>
 
-						<!-- Sidebar (Author) -->
-						<div class="col-12 col-lg-4">
-							<div class="sidebar">
-								<div class="row">
-									<div class="col-12 section-title">
-										<h2>About Creator(s)</h2>
-									</div>
-								</div>
-								<?php if ( is_array($creator_arr) && count($creator_arr)): ?>
-									<?php foreach ($creator_arr as $key => $value): ?>
-										<div class="row">
-											<div class="col-3">
-												<img class="rounded-circle w-100" src="<?= ($value['image']['url']) ? $value['image']['url'] : '' ; ?>">			
-											</div>
-											<div class="col-9 wc-creator-section">
-												<h3><?= $value['name']; ?></h3>
-												<strong><?= $value['role']; ?></strong>
-												<?= $value['description']; ?>
-											</div>
-										</div>
-										<br>
-									<?php endforeach; ?>
-								<?php endif; ?>
-							</div>
-						</div>
-					</div>
-				</section>
-			</div>
-		</main>
-	</main><!-- #main -->
+                        <!-- Sidebar (Author) -->
+                        <div class="col-12 col-lg-3">
+                            <div class="sidebar">
+                                <div class="row">
+                                    <div class="col-12 section-title">
+                                        <h2>Creator(s)</h2>
+                                    </div>
+                                </div>
+                                <?php if ( is_array( $creator_arr ) && count( $creator_arr ) ): ?>
+                                    <?php foreach ( $creator_arr as $key => $value ): ?>
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <img alt="<?= $value['name']; ?>" class="rounded-circle w-100" src="<?= ( $value['image']['url'] ) ? $value['image']['url'] : ''; ?>">
+                                            </div>
+                                            <div class="col-9 wc-creator-section">
+                                                <h3><?= $value['name']; ?></h3>
+                                                <strong><?= $value['role']; ?></strong>
+                                                <?= $value['description']; ?>
+                                            </div>
+                                        </div>
+                                        <br>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    </main><!-- #main -->
 </div><!-- #primary -->
