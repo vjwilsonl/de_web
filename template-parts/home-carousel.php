61175f7eb3bfa4c00f6ac4d823b1d5f3f674ae1b
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2019-04-18 02:13:36 +0800

    Updated Home Page Carousel

--- template-parts/home-carousel.php
+++ template-parts/home-carousel.php
@@ -1,61 +1,70 @@
-<?php 
-/**
- * The carousel for the homepage
- *
- * This is the template that displays carousel on home page
- *
- * @author wilson(vjwilsonL@gmail.com)
- *
- * @link    https://developer.wordpress.org/reference/functions/get_template_part/
- *
- * @package difference-engine
- */
+<?php
+    /**
+     * The carousel for the homepage
+     *
+     * This is the template that displays carousel on home page
+     *
+     * @author  wilson(vjwilsonL@gmail.com)
+     *
+     * @link    https://developer.wordpress.org/reference/functions/get_template_part/
+     *
+     * @package difference-engine
+     */
 ?>
-<!-- Carousel-->
-<?php if (class_exists('acf')): ?>
-<?php 
-	$carousel = get_field('carousel_images', 'option'); 
-	// d($carousel);
-?>
-<section class="section-carousel">
-	<div class="row no-gutters">
-		<div class="col-12">
-	   		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-			  <div class="carousel-inner">
-			  	<?php foreach ($carousel as $key => $value): ?>
-			  		<?php if (isset($value['poster']['url']) && $value['poster']['url']): ?>
-					    <div class="carousel-item <?= (!$key) ? 'active' : '' ?>">
-					    	
-					    	<?php if ($value['url']): ?>
-					    		<a href="<?= $value['url'] ?>">
-					    	<?php endif; ?>
-					    			<img class="d-block w-100" src="<?= $value['poster']['url']; ?>" alt="<?= $value['poster']['alt'] ?>">
-					    	<?php if ($value['url']): ?>
-					    		</a>
-					    	<?php endif; ?>
-					    </div>
-					<?php endif; ?>
-				<?php endforeach; ?>
-			  </div>
-			  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-			  	<span class="carousel-control-icon hvr-backward" aria-hidden="true"><i class="fas fa-chevron-left"></i></span>
-			    <!--<span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-			    <span class="sr-only">Previous</span>
-			  </a>
-			  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-			  	<span class="carousel-control-icon hvr-forward" aria-hidden="true"><i class="fas fa-chevron-right"></i></span>
-			    <!--<span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-			    <span class="sr-only">Next</span>
-			  </a>
-			  <ol class="carousel-indicators">
-			  	<?php foreach ($carousel as $key => $value): ?>
-			    	<li data-target="#carouselExampleIndicators" data-slide-to="<?= $key; ?>" 
-			    		class="<?= ($key == 0) ? 'active' : '' ; ?>">
-			    	</li>
-				<?php endforeach; ?>
-			  </ol>
-			</div>
-		</div>
-	</div>
-</section>
+    <!-- Carousel-->
+<?php if ( class_exists( 'acf' ) ): ?>
+    <?php $carousel = get_field( 'carousel_images', 'option' ); ?>
+    <section class="section-carousel">
+        <div class="row no-gutters">
+            <div class="col-12">
+                <div id="homeCarouselIndicators" class="carousel slide home-carousel" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <?php foreach ( $carousel as $key => $value ): ?>
+                            <?php if ( isset( $value['poster']['url'] ) && $value['poster']['url'] ): ?>
+                                <div class="carousel-item <?= ( ! $key ) ? 'active' : '' ?>">
+                                    <a href="<?= ($value['url']) ? $value['url'] : ''?>" class="<?= ($value['url']) ? '' : 'disabled' ?>" >
+                                        <img class="d-block w-100" src="<?= $value['poster']['url']; ?>" alt="<?= $value['poster']['alt'] ?>">
+                                    </a>
+                                    <div class="row no-gutters carousel-bottom">
+                                        <div class="col-1 text-center">
+                                            <p class="carousel-control">
+                                                <a class="" href="#homeCarouselIndicators" role="button" data-slide="prev">
+                                                    <span class="carousel-control-icon slide-left" aria-hidden="true">
+                                                        <i class="fas fa-arrow-left"></i>
+                                                    </span>
+                                                </a>
+                                            </p>
+                                        </div>
+                                        <div class="col-10 text-center">
+                                            <p class="carousel-title"><?= ($value['title']) ? $value['title'] : ''?></p>
+                                        </div>
+                                        <div class="col-1 text-center">
+                                            <p class="carousel-control">
+                                                <a class="" href="#homeCarouselIndicators" role="button" data-slide="next">
+                                                    <span class="carousel-control-icon slide-right" aria-hidden="true">
+                                                        <i class="fas fa-arrow-right"></i>
+                                                    </span>
+                                                </a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="row no-gutters">
+                        <div class="col-12">
+                            <ol class="carousel-indicators">
+                                <?php foreach ( $carousel as $key => $value ): ?>
+                                    <li data-target="#homeCarouselIndicators" data-slide-to="<?= $key; ?>"
+                                        class="<?= ( $key == 0 ) ? 'active' : ''; ?>">
+                                    </li>
+                                <?php endforeach; ?>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php endif; ?>
