b8a3a0b8c749146cd06706bdc3a4814b3d2df5d1
Vaibhav Sidapara <vaibhav@tusitalabooks.com>
2019-04-18 13:24:12 +0800

    Solved Search Results

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form action="/" method="get" class="form-inline">
     <input class="subnav-search form-control" type="text" placeholder="Search" name="s" id="search" value="<?php the_search_query(); ?>" aria-label="Search">
+    <input type="hidden" value="1" name="sentence" id="sentence" />
     <button class="fa-button" type="submit"><i class="fas fa-search"></i></button>
 </form>
